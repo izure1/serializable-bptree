5585687e10f83e27b02377723c7ddfb7784cf3e5
izure <izure@naver.com>
2024-03-17 19:08:25 +0900

    feat: Export several basic types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-export type { BPTreeNode } from './base/BPTree'
-export type { SerializeStrategyHead } from './base/SerializeStrategy'
+export type { BPTreeNode, BPTreeCondition } from './base/BPTree'
+export type { SerializeStrategyHead, SerializableData } from './base/SerializeStrategy'
 export { ValueComparator, NumericComparator, StringComparator } from './base/ValueComparator'
 export { BPTreeSync } from './BPTreeSync'
 export { BPTreeAsync } from './BPTreeAsync'
